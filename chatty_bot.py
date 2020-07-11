cb1c292498d984d41b73133826631d8e0d39fa5b
Alejandro Becerra <55593696+becerra2906@users.noreply.github.com>
2020-07-11 08:08:39 -0500

    Create chatty_bot.py

---
+++ chatty_bot.py
@@ -0,0 +1,23 @@
+### basic chat bot project done for jetbrains academy python learning route
+
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+message = "Your age is {}; that's a good time to start programming!".format(age)
+
+print(message)
